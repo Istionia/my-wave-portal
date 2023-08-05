4bc3fb038cc255edfba74b4a72a7811e8728b380
Tim Yoong <79699038+Istionia@users.noreply.github.com>
2023-08-06 01:35:13 +0800

    Add files via upload

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+import "hardhat/console.sol";
+
+pragma solidity ^0.8.19;
+
+contract WavePortal {
+    constructor() {
+        console.log("Well, heave her up and away we'll go");
+    }
+}
